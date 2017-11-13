b38c62d7d192ddd47cbfd830a82c12906332561b
Matthew Carter <m@ahungry.com>
2017-11-12 23:07:11 -0500

    Update the patch list

--- p99-launcher-generate-json.sh
+++ p99-launcher-generate-json.sh
@@ -4,19 +4,19 @@
 
 # Run this in the top level of an unzipped p99 zip set of files
 
-IFS=$'\n' 
+IFS=$'\n'
 
 # The filesize for the download zip file
 FILESIZE=$(ls -l *zip | awk '{print $5}')
 
 JSON=$(echo -e "[\n")
 
-for f in $(find -type f); do 
+for f in $(find -type f); do
 	if [ -z "$(echo $f | grep -E '\.zip$')" ]; then
 		JSON=$(echo -e "$JSON"; echo -e "    {")
 		JSON=$(echo -e "$JSON"; echo -e "        'filename':'$(echo $f|sed -e 's/\.\///g')',")
 		JSON=$(echo -e "$JSON"; echo -e "        'checksum':'$(md5sum $f|awk '{print $1}')',")
-		JSON=$(echo -e "$JSON"; echo -e "        'download':'http://www.project1999.com/files/P99Files39.zip',")
+		JSON=$(echo -e "$JSON"; echo -e "        'download':'http://www.project1999.com/files/P99Files45.zip',")
 		JSON=$(echo -e "$JSON"; echo -e "        'filesize':'${FILESIZE}'")
 		JSON=$(echo -e "$JSON"; echo -e "    },")
 	fi
